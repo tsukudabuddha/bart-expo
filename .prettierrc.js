dd976892e34ae75e6caea9ef437df4ba6b93aaa7
Andrew Tsukuda <andrewtsukuda@gmail.com>
2023-04-25 20:26:25 -0700

    add formatting

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    useTabs: false,
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    printWidth: 140,
+    arrowParens: 'avoid',
+    endOfLine: 'auto',
+}
---
+++ eslintrc.js
@@ -0,0 +1,90 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['import', 'better-styled-components', 'jest'],
+  extends: [
+      '@react-native-community',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+      'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      ecmaFeatures: {
+          jsx: true,
+      },
+  },
+  rules: {
+      indent: 'off',
+      curly: ['warn', 'all'],
+      'prettier/prettier': 'warn',
+      '@typescript-eslint/indent': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/explicit-member-accessibility': ['warn', { accessibility: 'no-public' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/member-delimiter-style': ['warn', { multiline: { delimiter: 'none' } }],
+      '@typescript-eslint/no-object-literal-type-assertion': 'off',
+      '@typescript-eslint/prefer-interface': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-extra-semi': 'off',
+      '@typescript-eslint/no-empty-function': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { varsIgnorePattern: 'h' }],
+      'no-console': 'warn',
+      semi: 'off',
+      'better-styled-components/sort-declarations-alphabetically': 'off',
+      'react/no-unstable-nested-components': 'off',
+      'import/order': [
+          'error',
+          {
+              alphabetize: {
+                  order: 'asc',
+              },
+              groups: [
+                  ['builtin', 'external', 'internal'],
+                  ['parent', 'sibling', 'index', 'unknown'],
+              ],
+              'newlines-between': 'always',
+          },
+      ],
+      'padding-line-between-statements': [
+          'warn',
+          {
+              blankLine: 'always',
+              prev: '*',
+              next: ['block-like', 'multiline-expression', 'multiline-const', 'return'],
+          },
+          {
+              blankLine: 'always',
+              prev: ['block-like', 'multiline-expression', 'multiline-const'],
+              next: '*',
+          },
+      ],
+      'react/prop-types': 'off',
+      'react/self-closing-comp': ['warn', { component: true, html: true }],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/display-name': 'off',
+      'react-hooks/exhaustive-deps': [
+          'warn',
+          {
+              additionalHooks: '(useAnimatedStyle)',
+          },
+      ],
+  },
+  overrides: [
+      {
+          files: ['*.js'],
+          rules: {
+              '@typescript-eslint/no-var-requires': 'off',
+          },
+      },
+  ],
+  settings: {
+      react: {
+          version: 'detect',
+      },
+  },
+}
